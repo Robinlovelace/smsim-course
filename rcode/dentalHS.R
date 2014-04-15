934e76033a8ef2d3ff2375b8ff67f43ec5a3628d
Robin <rob00x@gmail.com>
2014-04-15 16:08:04 +0100

    all individuals

--- rcode/dentalHS.R
+++ rcode/dentalHS.R
@@ -26,9 +26,28 @@ adhs <- adhs[c("NCakes", # how often eats cakes?
 #                  8 Never worked and long term unemployed
 #                  97 Not classified   
                "ageband4")] # grouped age - 7 cats  16 to 24, 25 to 34, 35 to 44, 45 to 54, 55 to 64, 65 to 74, 75 and over
-unique(adhs$NSSEC8)
 
+unique(adhs$NSSEC8)
+sapply(adhs, class) # note the variables are all numeric
+head(adhs)
 
 # Re-code to categorical
+library(car) # package for recode function
+ind <- adhs
+ind$NCakes <- recode(ind$NCakes, "1='6+';
+2='3-5'; 3='1-2'; 4='<1'; 5='rarely'", as.factor.result = T)
+head(ind$NCakes)
 
+head(ind$ageband4)
+ind$ageband4 <- recode(ind$ageband4, "1='16-24'; 2='25-34'; 3='35-44';
+                       4='45-54'; 5='55-64'; 6='65-74'; 7='75+'", as.factor.result = T)
+summary(ind$ageband4)
+
+ind$NSSEC8 <- as.factor(ind$NSSEC8)
+summary(ind$NSSEC8)
+
+ind$Car <- as.factor(ind$Car)
+summary(ind$Car) # no car is 2
+
+write.csv(ind, "cakeMap/ind.csv", row.names = F)
 
